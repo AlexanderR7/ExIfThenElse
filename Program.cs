9df73934729b38a5405bb58b17c1f12b46d73521
AlexanderR7 <67521417+AlexanderR7@users.noreply.github.com>
2022-12-30 13:48:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿static string PolygonSides1(int sideNum)                         // Declaring a method / function with a return type of string.
+{
+    if (sideNum > 3)                                                         // Boolean condition that is to be evaluated.
+    {
+        return "This shape is a polygon";                           // If condition is true, this statement is returned.
+    }
+    else
+    {
+
+        return "The shape is not a polygon";                     // Otherwise this statement is returned.
+    }
+
+}
+string sideNum = (PolygonSides1(4));               // Usage: Notice that "SideNum" can be used again here because
+Console.WriteLine(sideNum);                                      //first usage is not in scope. 
